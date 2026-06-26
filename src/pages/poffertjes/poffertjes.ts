01695254052c69dff02476ebddf424079d45192e
KingGizza <maaikerietdijk@gmail.com>
2026-06-26 17:14:03 +0200

    poffertjes datepricker verwijdert

--- src/pages/poffertjes/poffertjes.ts
+++ src/pages/poffertjes/poffertjes.ts
@@ -1,24 +1,13 @@
-import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
-import {MatFormField, MatHint, MatInput, MatLabel, MatSuffix} from '@angular/material/input';
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {provideNativeDateAdapter} from '@angular/material/core';
+
+import {Component} from '@angular/core';
+
 
 
 export class DatepickerOverviewExample {}
 @Component({
   selector: 'app-poffertjes',
   imports: [
-    MatDatepicker,
-    MatDatepickerInput,
-    MatDatepickerToggle,
-    MatFormField,
-    MatHint,
-    MatInput,
-    MatLabel,
-    MatSuffix
+
   ],
   templateUrl: './poffertjes.html',
   styleUrl: './poffertjes.scss',
